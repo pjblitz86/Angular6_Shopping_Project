9a1bb20db2cb9e3d8c6ca554b3d3e1b5431024ff
Paulius Juknis <paulius.juknis@gmail.com>
2018-05-31 13:23:13 +0300

    added shopping-list service as a singleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,10 +27,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
     DropdownDirective
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,29 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from './../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This simply a test',
+    new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty Schnitzel - just awesome!',
     // tslint:disable-next-line:max-line-length
-    'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'),
-    new Recipe('Another Recipe', 'This simply a test',
-    'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis')
+        'https://www.recipetineats.com/wp-content/uploads/2017/08/Schnitzel-3-landscape.jpg',
+        [
+           new Ingredient('Meat', 1),
+           new Ingredient('French Fries', 20)
+        ]),
+    new Recipe(
+        'Big Fat Burger',
+        'What else to say? King-size!',
+        // tslint:disable-next-line:max-line-length
+        'http://www.munchkintime.com/wp-content/uploads/2017/06/Best-Hamburger-Recipe-to-make-for-Fathers-Day-from-Munchkintime.com-46.jpg',
+        [
+           new Ingredient('Buns', 2),
+           new Ingredient('Meat', 1)
+        ])
   ];
 
   getRecipes() {

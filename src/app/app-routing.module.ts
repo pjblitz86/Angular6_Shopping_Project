fc8f9f0ddbaa607fd20f935e15d307fe08499ade
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-05 10:28:26 +0300

    added child routes and refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-    { path: 'recipes', component: RecipesComponent},
-    { path: 'shopping-list', component: ShoppingListComponent},
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'}
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    { path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent, pathMatch: 'full'},
+        {path: ':id', component: RecipeDetailComponent }
+    ] },
+    { path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({

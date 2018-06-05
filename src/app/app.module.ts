f52ed06404136b140cc24d5690809198701c6d42
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-05 09:24:28 +0300

    added routing instead of ngIf and refactored some htmls in order to eliminate page reload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -28,7 +29,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   ],
   imports: [
   BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]

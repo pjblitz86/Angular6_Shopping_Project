6a88e75bff9fc727831654266437f3d5377024fa
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-22 14:45:10 +0300

    refactoring auth and its' routes into seperate module

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,18 @@
+import { FormsModule } from '@angular/forms';
+import { ShoppingListComponent } from './shopping-list.component';
+import { NgModule } from '@angular/core';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+
+export class ShoppingListModule {}

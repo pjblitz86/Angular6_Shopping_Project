cf27a74fae73332da4b569bf987807a617eb1c40
Paulius Juknis <paulius.juknis@gmail.com>
2019-05-14 16:42:22 +0300

    fix getIdToken

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,43 +1,40 @@
-import { Router } from '@angular/router';
-import * as firebase from 'firebase';
-import 'firebase/auth';
-import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import * as firebase from "firebase";
+import "firebase/auth";
+import { Injectable } from "@angular/core";
 
 @Injectable()
-
 export class AuthService {
   token: string;
 
   constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .catch(
-        error => console.log(error)
-      );
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => console.log(error));
   }
 
   singinUser(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-        response => {
-          this.router.navigate(['/']);
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => this.token = token
-            );
-        }
-      )
-      .catch(
-        error => console.log(error)
-      );
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        this.router.navigate(["/"]);
+        firebase
+          .auth()
+          .currentUser.getIdToken()
+          .then((token: string) => (this.token = token));
+      })
+      .catch(error => console.log(error));
   }
 
-  getToken() {
-    firebase.auth().currentUser.getIdToken()
-      .then(
-        (token: string) => this.token = token
-      );
+  getIdToken() {
+    firebase
+      .auth()
+      .currentUser.getIdToken()
+      .then((token: string) => (this.token = token));
     return this.token;
   }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,42 +1,46 @@
-import { AuthService } from './../auth/auth.service';
-import { Recipe } from './../recipes/recipe.model';
-import { RecipeService } from './../recipes/recipe.service';
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
+import { AuthService } from "./../auth/auth.service";
+import { Recipe } from "./../recipes/recipe.model";
+import { RecipeService } from "./../recipes/recipe.service";
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/Rx";
 
 @Injectable()
-
 export class DataStorageService {
-  constructor(private http: Http,
-              private recipeService: RecipeService,
-              private authService: AuthService) {}
+  constructor(
+    private http: Http,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.http.put('https://ng-shopping-recipe-book.firebaseio.com/recipes.json?auth=' + token,
-                          this.recipeService.getRecipes());
+    const token = this.authService.getIdToken();
+    return this.http.put(
+      "https://ng-shopping-recipe-book.firebaseio.com/recipes.json?auth=" +
+        token,
+      this.recipeService.getRecipes()
+    );
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
+    const token = this.authService.getIdToken();
 
-    this.http.get('https://ng-shopping-recipe-book.firebaseio.com/recipes.json?auth=' + token)
-      .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
-          for (let recipe of recipes) {
-            if (!recipe['ingredients']) {
-              recipe['ingredients'] = [];
-            }
-          }
-          return recipes;
-        }
+    this.http
+      .get(
+        "https://ng-shopping-recipe-book.firebaseio.com/recipes.json?auth=" +
+          token
       )
-      .subscribe(
-        (recipes: Recipe[]) => {
-          this.recipeService.setRecipes(recipes);
+      .map((response: Response) => {
+        const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe["ingredients"]) {
+            recipe["ingredients"] = [];
+          }
         }
-      );
+        return recipes;
+      })
+      .subscribe((recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }

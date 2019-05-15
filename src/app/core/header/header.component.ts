dc99bc31d49c2a0603d362db81d49e8fd15b2c7b
Paulius Juknis <paulius.juknis@gmail.com>
2019-05-15 14:08:35 +0300

    auth fixes

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,31 +1,29 @@
-import { DataStorageService } from './../../shared/data-storage.service';
-import { AuthService } from './../../auth/auth.service';
-import { Component } from '@angular/core';
-import { Response } from '@angular/http';
+import { DataStorageService } from "./../../shared/data-storage.service";
+import { AuthService } from "./../../auth/auth.service";
+import { Component } from "@angular/core";
+import { Response } from "@angular/http";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
-
 export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService,
-                private authService: AuthService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
-    onSaveData() {
-      this.dataStorageService.storeRecipes()
-        .subscribe(
-            (response: Response) => {
-                console.log(response);
-            }
-        );
-    }
+  onSaveData() {
+    this.dataStorageService.storeRecipes().subscribe((response: Response) => {
+      console.log(response);
+    });
+  }
 
-    onFetchData() {
-       this.dataStorageService.getRecipes();
-    }
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 
-    onLogout() {
-        this.authService.logout();
-    }
+  onLogout() {
+    this.authService.logout();
+  }
 }
